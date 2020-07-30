8b7117540b49a4d9437c5a0429a801089e9fbf79
Parshant Sharma <parshantsharma360@gmail.com>
2020-07-30 15:36:45 +0530

    Updates

---
+++ precache-manifest.130451957893c7175a45225499e94b4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f6ad9caf49951fc6c63579d5f26c12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b61fc130ee8929d7a2ee",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "70f9f3148b287db57dee",
+    "url": "/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "b61fc130ee8929d7a2ee",
+    "url": "/static/js/2.81b10bf1.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.81b10bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70f9f3148b287db57dee",
+    "url": "/static/js/main.64761fab.chunk.js"
+  },
+  {
+    "revision": "b71fc92e9b0871852091",
+    "url": "/static/js/runtime-main.b8485ccb.js"
+  }
+]);
